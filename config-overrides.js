3181dc8d5b3d4014b6105ff8f8bec80e74b98688
watchcloud <javascrip_t@163.com>
2019-07-05 16:03:16 +0800

    test

---
+++ config-overrides.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const { override, fixBabelImports, addWebpackAlias } = require("customize-cra");
+
+module.exports = override(
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: "css"
+  }),
+  addWebpackAlias({
+    "@": path.resolve(__dirname, "src")
+  })
+);
